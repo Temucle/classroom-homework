b828231458be5cbe441b25bb86d5991fa7209726
Temucle <temujintemka0812@gmail.com>
2022-12-09 14:49:25 +0800

    new

--- JavaScript/primenumber/prime.js
+++ JavaScript/primenumber/prime.js
@@ -1,20 +1,18 @@
 let input = +prompt('Таны оруулсан тоог анхны')
 let result = ""
 for( i=2; i < input; i++ ) {
-    if ( input == 2 ) {
-        result = 'Энэ нь анхны тоо байна.'
-        break
-    }
-    if ( input == 0 || input == 1) {
-        result = 'Энэ нь анхны тоо биш байна.'
-        break
-    }
-    else if ( input%i == 0 ) {
+    if ( input%i == 0 ) {
         result = 'Энэ тоо нь анхны тоо биш байна.'
         break
     }
     else {
         result = 'Энэ нь анхны тоо байна.'
     }
+}
+if( input == 0 || input == 1) {
+    result = 'Энэ нь анхны тоо биш байна.'
+}
+else if ( input == 2 ) {
+    result = 'Энэ нь анхны тоо байна.'
 }
 alert( result )
